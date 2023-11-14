4cb1ab099e8c664691279652d2d374cba1b2192e
Hankey84 <hankeybanister84@gmail.com>
2023-11-14 14:02:55 +0300

    Создали примитивный поиск по тегам

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = 'https://scrapingclub.com/exercise/list_basic/?page=1'
+
+responce = requests.get(url)
+
+soup = BeautifulSoup(responce.text, "lxml") #Ещё один аналог lxml - htmll.parserб они нужны для структурирования HTML для лучшего поиска
+
+data = soup.find("div", class_="w-full rounded border")
+
+
+name = data.find("h4").text.replace("\n", "")
+price = data.find("h5").text
+url_img = "https://scrapingclub.com" + data.find("img", class_="card-img-top img-fluid").get("src")
+
+#print(data)
+print(name + "\n" + price +"\n" + url_img+ "\n\n")
