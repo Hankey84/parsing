73e76eb183b4af1a4d6a84acfc8672c9e86d6dc8
Hankey84 <hankeybanister84@gmail.com>
2023-11-15 12:41:53 +0300

    Добавил функцию сохранения в XL

--- main.py
+++ main.py
@@ -2,36 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-list_card_url = []
 
 # Делаем липовый заголовок, ятобы сайты не поняли что запрос от бота
 headers = {"User-Agent":
            "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)"}
 
-for count in range(1, 8): # У нас всего 7 вэб-страниц и их нужно все обработать
+def get_url(): # Создали функцию - генератор страниц, чтобы не захламлять опер.память списками 
+    for count in range(1, 8): # У нас всего 7 вэб-страниц и их нужно все обработать
 
-    sleep(3) # Задержка в 3 секунды, чтобы сайт не положить запросами
-    url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
+        url = f'https://scrapingclub.com/exercise/list_basic/?page={count}'
 
-    responce = requests.get(url, headers=headers)
+        responce = requests.get(url, headers=headers)
 
-    soup = BeautifulSoup(responce.text, "lxml") #Ещё один аналог lxml - htmll.parserб они нужны для структурирования HTML для лучшего поиска
+        soup = BeautifulSoup(responce.text, "lxml") #Ещё один аналог lxml - htmll.parserб они нужны для структурирования HTML для лучшего поиска
 
-    data = soup.find_all("div", class_="w-full rounded border")
+        data = soup.find_all("div", class_="w-full rounded border")
 
-    for i in data: #Делаем список из страниц с товаром для последующего разбора
-        card_url = "https://scrapingclub.com" + i.find("a").get("href")
-        list_card_url.append(card_url)
+        for i in data: #Делаем список из страниц с товаром для последующего разбора
+            card_url = "https://scrapingclub.com" + i.find("a").get("href")
+            yield card_url
 
-for card_url in list_card_url: # Цикл для проведения разбора на текущей странице
-    responce = requests.get(card_url, headers=headers)
-    soup = BeautifulSoup(responce.text, "lxml") 
-    data = soup.find("div", class_="my-8 w-full rounded border")
+def array(): # Создаём ещё одну ф-ю генератор, через которую будем записывать данные в XL
+    for card_url in get_url(): # Цикл для проведения разбора на текущей странице
+        responce = requests.get(card_url, headers=headers)
 
-    name = data.find("h3", class_="card-title").text
-    price = data.find("h4").text
-    description = data.find("p", class_="card-description").text
-    url_img = "https://scrapingclub.com" + data.find("img").get("src")
+        sleep(1) # Задержка в n секунды, чтобы сайт не положить запросами
+        soup = BeautifulSoup(responce.text, "lxml") 
+        data = soup.find("div", class_="my-8 w-full rounded border")
 
-
-    print(name + "\n" + price +"\n" + url_img + "\n" + description + "\n\n")
+        name = data.find("h3", class_="card-title").text
+        price = data.find("h4").text
+        description = data.find("p", class_="card-description").text
+        url_img = "https://scrapingclub.com" + data.find("img").get("src")
+        yield name, price, description, url_img
